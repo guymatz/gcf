ada953cf3ee4f7bd7de8c07e1385fab6a017539b
Guy Matz <gmatz@matz.org>
2014-03-29 23:01:02 -0400

    all functions in placce, just need to write them

--- gcf.py
+++ gcf.py
@@ -8,4 +8,25 @@ if len(sys.argv) < 3:
 
 num1 = sys.argv[1]
 num2 = sys.argv[2]
+
+def gcf(num1, num2):
+	# Sadie said to find prime factors
+	num1_pfs = find_prime_factors(num1)
+	num2_pfs = find_prime_factors(num2)
+
+        # She then said to find their common factors
+	cfs = find_common_factors(num1_pfs, num2_pfs)
+
+	# And then finally multiply the common factors
+	return multiply_common_factors(cfs)
+
+def find_prime_factors(num):
+	pass
+
+def find_common_factors(num1_pfs, num2_pfs):
+	pass
+
+def multiply_common_factors(cfs):
+	pass
+
 print gcf(num1, num2)
